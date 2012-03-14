520c5dbb685de44ee314519a60341f9cd4fb6610
Bruno Macherel <Bruno Macherel@b6b17452-1c8b-ef94-15eb-1b5c84adfae8>
2012-03-14 15:54:14 +0000

    adding freemobile authentication mechanism

---
+++ trunk/common/freemobile.js
@@ -0,0 +1,144 @@
+var hash = {
+"76a7a44ea96ff2dc87cec9c9b24b6b772fa7fd33":0,
+"7b3e643e1d04d3e90d5ddf8ce9b1566b2fd3ac51":0,
+"f492cefd432deebacf68770752f0816181f232cf":0,
+"ffedf7a9efd3757c350e90d40738d830b8186002":0,
+"77e27cfcd27b784cf3142e11db7295689ebe22ba":1,
+"4c9330ca33b667309dd9a0571b47f5845c631d54":1,
+"d0fd218cbcbdc13bc0fb9aa16ae943d683f37fbc":1,
+"005652cf9d1c2c15f32f62c394c49f1dcdcb9fff":2,
+"08b80e702cbd41ba70b96a48fb4c775bd35d2708":2,
+"de0c9b9f4f1e35d3fb3e22d48cd11af8ded60051":2,
+"c66de6f761fedae876518576e0f0b653fc3abdb2":2,
+"c284e3ffd791e1b0a491cb534481bb71b7ad17dd":2,
+"0efc942027cee74f1830c1e3ba8b97a9fab3d7e7":2,
+"26a73f7f756d72d1aca233e359d2ba1ac264e491":2,
+"bafe86c2dd37eff1f6ad08fefd0c6ba0a35573da":2,
+"a402d144e7e1385631ae7d64fa3fce347269e5d8":2,
+"66847c85225947fc0f21ae3e07796a7b27d6a113":3,
+"fcfc2b517a416d26a49aa1ea47044972df457164":3,
+"582459c8413e004354b58a94fd32b31a564751cc":3,
+"ceccfc128903c13c5e8d1e96b1c843d57067ec82":3,
+"0260ee7b65fd44b61b5d0a5c45079d3b00fdc397":3,
+"29d8ddf87ff231df34bb94366d4c4b8a285cb575":3,
+"675162ba677e6de6ed38373bdcda4ec283f2e481":3,
+"5960134db5e22e2c00485c9c458ab39c0803c23e":3,
+"ea8e5f99147e66c798df9c86897ab29d6f5fcb71":3,
+"de2560b6c674953572fbd1ebfdafccc5b7d4e572":4,
+"cceb0e1d53d97d8b902ac28f8d5d3394e9fadf8d":4,
+"c4a7b5212ff609a56d883381e3dc3da79016711a":4,
+"fb23fcabdd08c47c1bdb2f1e936fd8bf0a0f41b5":4,
+"5ec5f274dd64f70741c0fc83055db68863d361ca":4,
+"d6aa675e754dfbfe6160d2c48e6d87f41fb5c0fb":4,
+"547c20f7b62a13240a18c599d9e8644c76772645":4,
+"8384c8326ea594dc1a01ae33fea9306f0447eb80":5,
+"cb65b807c80b501356fe92d40ac62c20373e6798":5,
+"fbb887b39291dd7b16516e840960a840d51dcf06":5,
+"c639e84563218cdbf83a0b5c3238979c67902e05":5,
+"e445e33db35b90264c8c0dc28cd803e80a6f9d3f":5,
+"0816e67caa40edf56fd97c57d14615c7e91be4e0":5,
+"36273cad72acada4b9b93b4406913b8109a02b7a":5,
+"19f8e1421da19146292ccc6a0bf9d910d78cbd2c":5,
+"4d1badf9671555cdb0b0d7577f802c2d187ed871":5,
+"18dd59d3fe3739cf94202956c3e5c6d049f34ad2":6,
+"2ed29c9786ea98ad1a1aa65bac5baa579377ac78":6,
+"30c7e0e7d96f129f4a24685517951fff505d5f2d":6,
+"38546c088e1f534f597b3f64f6ef251913d59a6f":6,
+"6a702323e4423a430cbe0ada96fc41ee0ee852ed":6,
+"87ecfbcd6d7c6bcfe464994ef82af842bccfb897":6,
+"93413e3b6ceaf564be85e3ccfce95ece13d14727":6,
+"99cdec4b704e118e9a9f7e632e824e8bcd3eda69":6,
+"e61f86a60d3796b0948fb5892d26acff648b806e":6,
+"9150feb3785fe6e0063e1e28b777ec36feeb3aa4":6,
+"ba492edda131c7a99fec067590af386c46e68555":7,
+"a9a39ce2def8c64f2a8ef79f4f2ce22bd11a9440":7,
+"39ee908dc08928a01fc9cba93a0a1266239caf78":7,
+"0f625de3bbf2d1219eda27d75c63526df2432ac2":7,
+"19202787254f586ad3b74c945457f24e0287a68a":7,
+"628275a4447baae9eda8f8132de92c73336107e8":7,
+"14f3c0ea88e52176e7745a59627ecda39321cb35":8,
+"33e466c363c4b24dbfb2bc4defd614a85ee6193f":8,
+"5c2a958213805db35576006ea2ad7e7c2258208d":8,
+"65a6b48912f88b8ee3e0b054d02682a0defe7f02":8,
+"72e3c03bf3ffdd5be1e295b078fbb4f5b6ba6e76":8,
+"7a1ec06d238fa5d8576ebacc7463ba5f3c824880":8,
+"9a655efa2d4a826e4525087a6c607998aa9d9606":8,
+"a4d81cf65a4828f0f25409f313604a06253e8037":8,
+"aa3f66681e9721cbc30bd4ad5e554701cab0463c":8,
+"f218c4cbc405d51c19bc2de8a23507b3451e6567":8,
+"e00f0071cba7240d17edb230205b760c4cf0f7fa":9,
+"f1fb86f19b4708bdf4468248bd1709d736324fb4":9,
+"d10606b3148341dea7bc15023796274b4cbdcf2c":9,
+"6e2169f0035afe4b33f6413768a3a837bde14171":9,
+"44c52351890f0a7c19fdbf7582a3bd2c886e2d25":9,
+"d2753c90a29785bee5142da61b834112dc0435d0":9,
+"9e97228f54a2f2d94cd09c96c7c1c0049b6879b7":9,
+"80a641ba1027a51ce89b52b1e06acc583cf6bca4":9,
+"7cd18ec91abac1d1dcdca37c097259bb4bf1da23":9,
+"63a0f504a1990aae88c33dcdee3e3a5bac96beff":9
+};
+
+var pos = [];
+
+function ocr() {
+  for(var i=0; i<10; i++) {
+    var img = document.getElementsByClassName('ident_chiffre_img')[i];
+    var canvas = getCanvas(img);
+    canvas = convert(canvas);
+    var sha = SHA1(canvas.toDataURL("image/png").replace(/^data:image\/(png|jpg);base64,/, ""));
+    var n = hash[sha];
+    console.log(n!=undefined?n:sha);
+    if(n != undefined) {
+      pos[n] = i;
+    }
+  }
+}
+
+function convert(canvas) {
+  var image = canvas.getContext("2d").getImageData(0, 0, canvas.width, canvas.height);
+  var data = image.data;
+  for(var y=0; y<canvas.height; y++) {
+    for(var x=0; x<canvas.width; x++) {
+      var i = x*4+y*4*canvas.width;
+      var grayscale = data[i  ] * .3 + data[i+1] * .59 + data[i+2] * .11;
+      grayscale = (grayscale==255||x<10||x>30||y<10||y>30)?255:0;
+      data[i  ] = grayscale;   // red
+      data[i+1] = grayscale;   // green
+      data[i+2] = grayscale;   // blue
+      data[i+3] = 255;         // alpha
+    }
+  }
+  for (var i = 0, n = data.length; i < n; i += 4) {
+  }
+  canvas.getContext("2d").putImageData(image, 0, 0);
+  return canvas;
+}
+
+function getCanvas(img) {
+    // Create an empty canvas element
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    // Copy the image contents to the canvas
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    
+    return canvas
+}
+
+function authenticate(credential) {
+  for(var i in credential.username) {
+    var n = parseInt(credential.username[i]);
+    var p = pos[n];
+    document.getElementsByClassName('ident_chiffre_img')[p].onclick();
+  }
+  document.getElementsByName("pwd_abo")[0].value = credential.password;
+  document.getElementById("ident_btn_submit").click();
+}
+
+function process(credential) {
+  ocr();
+  authenticate(credential);
+}
+
