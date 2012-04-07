ee0eedf998c50861200324bbb7c78c5467dd0808
Bruno Macherel <Bruno Macherel@b6b17452-1c8b-ef94-15eb-1b5c84adfae8>
2012-04-07 20:03:41 +0000

    Modification de l'injection des scripts sous chrome pour intercepter les pages se chargeant dans des frames

--- trunk/chrome/src/background_page/background.js
+++ trunk/chrome/src/background_page/background.js
@@ -2,44 +2,51 @@
  * Inject all needed script in the page
  */
 function injectScripts(tabId, script_config, config) {
-  var credential = config.credential;
+	var credential = config.credential;
 
-  // Inject library scripts
-  for(var i in script_config.libs) {
-    chrome.tabs.executeScript(tabId, {file: "scripts/" + script_config.libs[i]});
-  }
-  // Inject others specifics scripts
-  for(var i in script_config.files) {
-    chrome.tabs.executeScript(tabId, {file: "scripts/" + script_config.id + "/" + script_config.files[i]});
-  }
-  // Inject the main script and launch it
-  chrome.tabs.executeScript(tabId, {file: "scripts/" + script_config.id + "/script.js"}, function() {
-    chrome.tabs.executeScript(tabId, {code: "execute("+JSON.stringify(credential)+")"});
-  });
+	// Inject library scripts
+	for(var i in script_config.libs) {
+		chrome.tabs.executeScript(tabId, {file: "scripts/" + script_config.libs[i]});
+	}
+	// Inject others specifics scripts
+	for(var i in script_config.files) {
+		chrome.tabs.executeScript(tabId, {file: "scripts/" + script_config.id + "/" + script_config.files[i]});
+	}
+	// Inject the main script and launch it
+	chrome.tabs.executeScript(tabId, {file: "scripts/" + script_config.id + "/script.js"}, function() {
+		chrome.tabs.executeScript(tabId, {code: "execute("+JSON.stringify(credential)+")"});
+	});
 }
 
 // Add a listener on tabs for injecting scripts if needed
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if(!!changeInfo && changeInfo.status=="complete") {
-    for(var i in SCRIPTS_CONFIG) {
-      var script_config = SCRIPTS_CONFIG[i];
-      var config = eval("("+window.localStorage.getItem(script_config.id)+")");
-      if(!config) continue;
-      if(!config.enabled) continue;
-      var pages = script_config.pages;
-      // loop on each page that can match
-      for(var n in pages) {
-        var page = pages[n];
-        page = page.replace(/\?/, "\\?");
-        page = page.replace(/\*/, ".*");
-        // Does the current URL match with this config ?
-        if(tab.url.match(page)) {
-          injectScripts(tabId, script_config, config);
-        }
-      }
-    }
-  }
-});
+var callback = function(details) {
+	console.log(details);
+	for(var i in SCRIPTS_CONFIG) {
+		var script_config = SCRIPTS_CONFIG[i];
+		var config = eval("("+window.localStorage.getItem(script_config.id)+")");
+		if(!config) continue;
+		if(!config.enabled) continue;
+		var pages = script_config.pages;
+		// loop on each page that can match
+		for(var n in pages) {
+			var page = pages[n];
+			page = page.replace(/\?/, "\\?");
+			page = page.replace(/\*/, ".*");
+			// Does the current URL match with this config ?
+			if(details.url.match(page)) {
+	console.log("##############################");
+				injectScripts(details.tabId, script_config, config);
+			}
+		}
+	}
+};
+var filter = {
+	urls: ["<all_urls>"],
+	types: ["main_frame", "sub_frame"]
+};
+var opt_extraInfoSpec = [
+];
+chrome.webRequest.onCompleted.addListener(callback, filter, opt_extraInfoSpec);
 
 function getOption(name) {
 	return eval("("+window.localStorage.getItem(name)+")");
