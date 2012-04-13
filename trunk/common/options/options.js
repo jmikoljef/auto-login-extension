5aecdcae3802a77d9b61e396699b674218c61992
Bruno Macherel <Bruno Macherel@b6b17452-1c8b-ef94-15eb-1b5c84adfae8>
2012-04-13 15:35:19 +0000

    Changement de l'architecture de la page d'option

--- trunk/common/options/options.js
+++ trunk/common/options/options.js
@@ -1,73 +1,6 @@
-function getPanelsDescr() {
-	var panelsDescr = {
-		main: {
-			description:'G\u00E9n\u00E9rales',
-			prefs: [
-				{
-					description: "Mode de notification",
-					options: [
-						{
-							id: "main.notification",
-							type: "radio",
-							values: [
-								{ value: "desktop", label: "Sur le bureau" },
-								{ value: "browser", label: "Dans le navigateur" },
-								{ value: "page", label: "Uniquement sur la page du site" }
-							],
-							default: "desktop"
-						}
-					]
-				}
-			]
-		}
-	};
-	for(var i=0; i<SCRIPTS_CONFIG.length; i++) {
-	  var config = SCRIPTS_CONFIG[i];
-	  panelDescr = {
-			description: config.label,
-			prefs: [
-				{
-					description: "Mode",
-					options: [
-						{
-							id: config.id + ".mode",
-							type: "radio",
-							values: [
-								{ value: 1, label: "Automatique" },
-								{ value: 2, label: "Manuel" }
-							],
-							default: 1
-						}
-					]
-				},
-				{
-					description: "Identifiant / mot de passe",
-					options: [
-						{id: config.id + ".credential.username", type: "text", label: "Identifiant"},
-						{id: config.id + ".credential.password", type: "password", label: "Mot de passe"}
-					]
-				}
-			]
-		};
-		panelsDescr[config.id] = panelDescr;
-	}
-	return panelsDescr;
+function init() {
+	switchPanel("main");
 }
-function getMenusDescr() {
-	var menusDescr = [];
-	var mainMenu =    { id: "main", label: "G\u00E9n\u00E9rales", img:"main.png" };
-	var scriptsMenu = { id: "scripts", label: "Scripts", img: "extension.png", submenu: [] };
-	for(var i=0; i<SCRIPTS_CONFIG.length; i++) {
-	  var config = SCRIPTS_CONFIG[i];
-	  scriptsMenu.submenu.push({id: config.id, label: config.label, img: "advanced.png", checkbox: true});
-	}
-	menusDescr.push(mainMenu);
-	menusDescr.push(scriptsMenu);
-	return menusDescr;
-}
-
-var panels = getPanelsDescr();
-var menus = getMenusDescr();
 
 function switchPanel(id) {
 	var tags = document.getElementsByClassName('menu-item-l1');
@@ -97,294 +30,31 @@ function switchPanel(id) {
 			pane.style.display = 'none';
 		}
 	}
-	restoreOptions(id);
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// functions for initializing panels and menu
-////////////////////////////////////////////////////////////////////////////////
-function createMenuItem(parent, menuItem, level) {
-  if(!level) level = 1;
-	var div = document.createElement("div");
-	div.setAttribute("id", "menu-"+menuItem.id);
-	div.setAttribute("class", "menu-disabled menu-item-l"+level);
-	var a = document.createElement("a");
-	a.setAttribute("href", "javascript:switchPanel('" + menuItem.id + "')");
-	var img = document.createElement("img");
-	img.setAttribute("src", menuItem.img);
-	a.appendChild(img);
-	var span = document.createElement("span");
-	var text = document.createTextNode(menuItem.label);
-	span.appendChild(text);
-	a.appendChild(span);
-	div.appendChild(a);
-	if(!!menuItem.checkbox) {
-		var checkbox = document.createElement("input");
-		checkbox.setAttribute("type", "checkbox");
-		var checkboxId = menuItem.id + ".enabled";
-		checkbox.setAttribute("name", checkboxId);
-		checkbox.setAttribute("id", checkboxId);
-		checkbox.setAttribute("onchange", "updateOption('"+checkboxId+"')");
-		div.appendChild(checkbox);
-	}
-	parent.appendChild(div);
-	if(!!menuItem.submenu) {
-	  for(var i=0; i<menuItem.submenu.length; i++) {
-	    createMenuItem(parent, menuItem.submenu[i], level + 1);
-	  }
-	}
-	var options = {};
-	var id = menuItem.id;
-	var panelItem = panels[id];
-	if(!!panelItem) {
-		createPanel(id, panelItem);
-		options[id] = getOption(id);
-		if(!!options[id]) restoreOption(options, id+".enabled");
-	}
-}
-
-function createPanel(id, panel) {
-	var parent = document.getElementById("body");
-	var panelDiv = document.createElement("div");
-	panelDiv.setAttribute("id", "panel-"+id);
-	panelDiv.setAttribute("class", "panel");
-	var title = document.createElement("h2");
-	title.appendChild(document.createTextNode(panel.description));
-	panelDiv.appendChild(title);
-	var element = document.createElement("div");
-	element.setAttribute("class", "element");
-	if(panel.prefs) {
-		for(var i=0; i<panel.prefs.length; i++) {
-			createPref(element, panel.prefs[i]);
-		}
-	}
-	panelDiv.appendChild(element);
-	var buttons = document.createElement("div");
-	buttons.setAttribute("class", "button");
-	var button = document.createElement("input");
-	button.setAttribute("name", "button");
-	button.setAttribute("type", "button");
-	button.setAttribute("value", "Enregistrer");
-	button.setAttribute("onclick", "saveOptions('"+id+"');");
-	buttons.appendChild(button);
-	panelDiv.appendChild(buttons);
-	parent.appendChild(panelDiv);
-}
-
-function createPref(parent, prefDescr) {
-	var fieldset = document.createElement("fieldset");
-	var legend = document.createElement("legend");
-	legend.appendChild(document.createTextNode(prefDescr.description));
-	fieldset.appendChild(legend);
-	for(var i=0; i<prefDescr.options.length; i++) {
-		createOption(fieldset, prefDescr.options[i]);
-	}
-	parent.appendChild(fieldset);
-}
-
-function createOption(parent, optionDescr) {
-	if(optionDescr.type=="radio") createRadio(parent, optionDescr);
-	if(optionDescr.type=="checkbox") createCheckbox(parent, optionDescr);
-	if(optionDescr.type=="text") createText(parent, optionDescr);
-	if(optionDescr.type=="password") createPassword(parent, optionDescr);
-}
-
-function createRadio(parent, optionDescr) {
-	for(var i=0; i<optionDescr.values.length; i++) {
-		var radio = optionDescr.values[i];
-		var input = document.createElement("input");
-		input.setAttribute("type", "radio");
-		input.setAttribute("name", optionDescr.id);
-		var id = optionDescr.id+"."+radio.value
-		input.setAttribute("id", id);
-		input.setAttribute("value", radio.value);
-		if(radio.value == optionDescr.default) {
-			input.setAttribute("checked", "checked");
-		}
-		input.setAttribute("class", "left");
-		parent.appendChild(input);
-		var label = document.createElement("label");
-		label.setAttribute("for", id);
-		label.setAttribute("class", "right");
-		label.appendChild(document.createTextNode(radio.label));
-		parent.appendChild(label);
-	}
-}
-function createCheckbox(parent, optionDescr) {
-}
-function createText(parent, optionDescr) {
-	var label = document.createElement("label");
-	label.setAttribute("for", optionDescr.id);
-	label.setAttribute("class", "left");
-	label.appendChild(document.createTextNode(optionDescr.label + " : "));
-	parent.appendChild(label);
-	var input = document.createElement("input");
-	input.setAttribute("type", "text");
-	input.setAttribute("name", optionDescr.id);
-	input.setAttribute("id", optionDescr.id);
-	input.setAttribute("class", "right");
-	parent.appendChild(input);
-}
-function createPassword(parent, optionDescr) {
-	var label = document.createElement("label");
-	label.setAttribute("for", optionDescr.id);
-	label.setAttribute("class", "left");
-	label.appendChild(document.createTextNode(optionDescr.label + " : "));
-	parent.appendChild(label);
-	var input = document.createElement("input");
-	input.setAttribute("type", "password");
-	input.setAttribute("name", optionDescr.id);
-	input.setAttribute("id", optionDescr.id);
-	input.setAttribute("class", "right");
-	parent.appendChild(input);
-}
-
-function initOptions() {
-	for(var i=0; i<menus.length; i++) {
-		var menu = menus[i];
-		var id = menu.id;
-		createMenuItem(document.getElementById("menu"), menu);
-	}
-	switchPanel("main");
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// functions for saving prefs
-////////////////////////////////////////////////////////////////////////////////
 function saveOptions(id) {
-	for(var pannelId in panels) {
-		var panel = panels[pannelId];
-		if(pannelId==id) {
-			var prefs = getPrefs(panel.prefs);
-			fillObject(prefs, id+".enabled");
-			if(!prefs) return;
-			saveObject(prefs[id], id);
+	var prefs = getOptionsFromPage("_alo_", id+".");
+	storeOptions(prefs[id], id);
+}
+
+function loadOption(option, id) {
+	if(options instanceof Object) {
+		for(var key in options) {
+			if(!id) {
+				id = key;
+			} else {
+				id += "." + key;
+			}
+			loadOption(option[key], id);
+		}
+	} else {
+		if(!id) {
+			console.error("Can't load option : id is not defined.");
 			return;
 		}
+		setElementValue(id, option);
 	}
 }
 
-function saveObject(object, name) {
-	setOption(name, object);
-}
+window.addEventListener("load", init, false);
 
-function getPrefs(prefs) {
-    if(!prefs) return;
-    var result = {};
-	for(var i=0; i<prefs.length; i++) {
-		var pref = prefs[i];
-		for(var j=0; j<pref.options.length; j++) {
-			fillObject(result, pref.options[j].id);
-		}
-	}
-	return result;
-}
-
-function fillObject(obj, name) {
-	var value = getValue(name);
-	var name_parts = name.split(".");
-	for(var i in name_parts) {
-		name = name_parts[i];
-		if(i!=name_parts.length-1) {
-			if(!obj[name]) obj[name] = {};
-			obj = obj[name];
-		}
-	}
-	obj[name] = value;
-}
-
-function getValue(name) {
-	var elements = document.getElementsByName(name);
-	if(elements.length==0) return;
-	if(elements[0].type=="radio") {
-		// Find the checked value
-		for(var i=0; i<elements.length; i++) {
-			if(elements[i].checked) {
-				return elements[i].value;
-			}
-		}
-	} else {
-		var values = [];
-		for(var i=0; i<elements.length; i++) {
-			if(elements[i].type=="checkbox") {
-				values.push(elements[i].checked);
-			} else {
-				values.push(elements[i].value);
-			}
-		}
-		if(elements.length==1) return values[0];
-		else return values;
-	}
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// functions for restoring prefs
-////////////////////////////////////////////////////////////////////////////////
-function restoreOptions(id) {
-	var panel = panels[id];
-	panel.id = id;
-	var options = {};
-	options[id] = getOption(id);
-	if(!options[id]) return;
-	restorePrefs(panel.prefs, options);
-	restoreOption(options, id+".enabled");
-}
-function restorePrefs(prefs, options) {
-	for(var i=0; i<prefs.length; i++) {
-		var pref = prefs[i];
-		for(var j=0; j<pref.options.length; j++) {
-			restoreOption(options, pref.options[j].id);
-		}
-	}
-}
-function restoreOption(options, name) {
-	// Find the value to restore
-	var value = options;
-	var name_parts = name.split(".");
-	for(var i in name_parts) {
-		value = value[name_parts[i]];
-	}
-	// restore the value
-	restoreValue(name, value);
-}
-function restoreValue(name, value) {
-	var elements = document.getElementsByName(name);
-	if(elements.length==0) return;
-	if(elements[0].type=="radio") {
-		for(var i=0; i<elements.length; i++) {
-			elements[i].checked = elements[i].value==value;
-		}
-	} else {
-		var values;
-		if(value instanceof Array) values = value;
-		else values = [ value ];
-		for(var i=0; i<elements.length; i++) {
-			if(elements[i].type=="checkbox") {
-				elements[i].checked=values[i];
-//				for(var j=0; j<values.length; j++) {
-//					elements[i].checked = elements[i].value==values[j];
-//				}
-			} else {
-				elements[i].value=values[i];
-			}
-		}
-	}
-}
-
-function updateOption(name) {
-	var name_parts = name.split(".");
-	var id = name_parts[0]
-	var options = {};
-	options[id] = getOption(id);
-	fillObject(options, name);
-	setOption(id, options[id]);
-}
- 
-function getOption(name) {
-	return eval("("+window.localStorage.getItem(name)+")");
-}
-function setOption(name, value) {
-	window.localStorage.setItem(name, JSON.stringify(value));
-}
-
-window.addEventListener("load", initOptions, false);
---
+++ trunk/common/options/settings.js
@@ -0,0 +1,126 @@
+/**
+ * Return options having a specific classname and having a name that stats whith prefix (optional)
+ */
+function getOptionsFromPage(classname, prefix) {
+	var prefs = {};
+	var elements = document.getElementsByClassName(classname);
+	for(var i=0; i<elements.length; i++) {
+		var element = elements[i];
+		var name = element.name;
+		// if there was a prefix, we check that the name starts with
+		if(!prefix || name.startWith(prefix)) {
+			var value = getElementValue(name);
+			fillObject(prefs, name, value);
+		}
+	}
+	return prefs;
+}
+
+/**
+ * Put the value in the json object corresponding to the name
+ * i.e: fillObject({ bar:{ foo: "value" } }, "foo.bar", "value") trasnform the object to { bar:{ foo: "value" }, foo:{ bar: "value" } }
+ */
+function fillObject(object, name, value) {
+	var name_parts = name.split(".");
+	for(var i in name_parts) {
+		name = name_parts[i];
+		if(i!=name_parts.length-1) {
+			if(!object[name]) object[name] = {};
+			object = object[name];
+		}
+	}
+	object[name] = value;
+}
+
+/**
+ * Return the value of an input element (text, password, radio, checkbox)
+ * If there was many element with this name, the result will be an array
+ */
+function getElementValue(name) {
+	var elements = document.getElementsByName(name);
+	if(elements.length==0) return;
+	if(elements[0].type=="radio") {
+		return _getRadioElementValue(elements);
+	} else if(elements[0].type=="checkbox") {
+		return _getRadioElementValue(elements);
+	} else {
+		_getDefaultElementValue(elements);
+	}
+}
+function _getRadioElementValue(elements) {
+	// Find the checked value
+	for(var i=0; i<elements.length; i++) {
+		if(elements[i].checked) {
+			return elements[i].value;
+		}
+	}
+}
+function _getCheckboxElementValue(elements) {
+	if(elements.length==1) {
+		return elements[0].checked;
+	} else {
+		var values = [];
+		for(var i=0; i<elements.length; i++) {
+			if(elements[i].checked) {
+				values.push(elements[i].value);
+			}
+		}
+		return values;
+	}
+}
+function _getDefaultElementValue(elements) {
+	if(elements.length==1) {
+		return elements[0].value;
+	} else {
+		var values = [];
+		for(var i=0; i<elements.length; i++) {
+			values.push(elements[i].value);
+		}
+		return values;
+	}
+}
+
+/**
+ * Set the value to an input element
+ */
+function setElementValue(name, value) {
+	var elements = document.getElementsByName(name);
+	if(elements.length==0) return;
+	if(elements[0].type=="radio") {
+		_setRadioElementValue(elements, value)
+	} else {
+		var values;
+		if(value instanceof Array) values = value;
+		else values = [ value ];
+		if(elements[0].type=="checkbox") {
+			_setCheckboxElementValue(elements, values);
+		} else {
+			_setDefaultElementValue(elements, values);
+		}
+	}
+}
+function _setRadioElementValue(elements, value) {
+	for(var i=0; i<elements.length; i++) {
+		elements[i].checked = elements[i].value==value;
+	}
+}
+function _setCheckboxElementValue(elements, values) {
+	if(elements.length==1) {
+		// if there's one element, it is treated as a boolean
+		elements[0].checked = !!elements[0].value;
+	} else {
+		for(var i=0; i<elements.length; i++) {
+			// We have to check if the checkbox value is included in values
+			elements[i].checked = false;
+			for(var j=0; j<values.length && !elements[i].checked; j++) {
+				elements[i].checked = elements[i].value==values[j];
+			}
+		}
+	}
+}
+function _setDefaultElementValue(elements, values) {
+	for(var i=0; i<elements.length; i++) {
+		elements[i].value=values[i];
+	}
+}
+
