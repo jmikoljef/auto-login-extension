e5ced10dd5c6f4ca707e857ea1a4d749b3676677
Bruno Macherel <Bruno Macherel@b6b17452-1c8b-ef94-15eb-1b5c84adfae8>
2012-04-24 20:44:20 +0000

    prise en compte de plusieurs langue possible

--- trunk/common/i18n/i18n.js
+++ trunk/common/i18n/i18n.js
@@ -1,28 +1,43 @@
-function i18n(locale, resolveUrl) {
-	this.locale;
+function i18n(locales, resolveUrl) {
+	this.locales = [];
+	if(!(locales instanceof Array)) {
+		locales = [ locales ];
+	}
+	for(var i = 0; i<locales.length; i++) {
+		var locale = this.getLocale(locales[i], resolveUrl);
+		if(locale != undefined) {
+			this.locales.push(locale);
+		}
+	}
+}
 
+i18n.prototype.getLocale = function(locale, resolveUrl) {
+	var result = undefined;
 	var url = "locale/"+locale+".json";
 	var file = new File(url, resolveUrl);
 	var opened = file.open();
 	if(opened) {
 		console.info("Using locale "+locale);
-		this.locale = file.getJsonContent();
+		result = file.getJsonContent();
 		file.close();
 	} else {
 		var i = locale.indexOf("_");
 		if(i != -1) {
 			var newLocale = locale.substring(0, i);
 			console.warn("File not found for " + locale + ", trying with " + newLocale + ".");
-			this.constructor(newLocale, resolveUrl);
+			result = this.getLocale(newLocale, resolveUrl);
 		} else {
 			console.warn("Locale not found, default messages will be used");
 		}
 	}
+	return result;
 }
+
 i18n.prototype.getMessage = function(message) {
+	var locale = this.locales[0];
 	var msg = undefined;
-	if(this.locale != undefined) {
-		msg = this.locale[message];
+	if(locale != undefined) {
+		msg = locale[message];
 		if(msg==undefined) {
 			console.warn("No translation found for \"" + message + "\"");
 			msg = message;
