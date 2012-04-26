8ba2494ad414d0369b676d9bc75de798f7def241
pierremarie.dhaussy <pierremarie.dhaussy@b6b17452-1c8b-ef94-15eb-1b5c84adfae8>
2012-04-26 22:10:14 +0000

    Fix : event mapped with Alex class New : cnx mode wrote

--- trunk/firefox/src/lib/alex.class.js
+++ trunk/firefox/src/lib/alex.class.js
@@ -1,70 +1,83 @@
 MODULE_NOTIFICATIONS = require('module-notifications');
 PASSWORD_MANAGER = require("password-manager");
+SIMPLE_STORAGE = require("simple-storage");
+STORAGE = SIMPLE_STORAGE.storage;
 Logger = require('logger');
 
-function AlexClass(scriptUrl, scriptWorker) {
+//Alex constructor
+exports.Alex = function (script, worker) {
+	Logger.debug('alex.class.js', 'exports.Alex');
+	return new Alex(script, worker);
+}
+
+function Alex(script, scriptWorker) {
 	Logger.debug('alex.class.js', 'Alex()');
-	this.url = scriptUrl;
+	this.script = script;
 	this.worker = scriptWorker;
     this.filled = false;
     this.validated = false;
-
-    this.fillForm = function () {
-    	Logger.debug('alex.class.js', 'fillForm');
-    	var theWorker = this.worker;
-	    PASSWORD_MANAGER.getFirst({
-		    url : this.url
-	    }, function(credential) {
-	    	theWorker.port.emit('fillForm', credential);
-	    });
-    }
-
-    this.validate = function validate() {
-    	Logger.debug('alex.class.js', 'validate');
-    	this.worker.port.emit('validate');
-    }
-
-    this.onFilled = function () {
-    	Logger.debug('alex.class.js', 'onFilled');
-	    this.filled = true;
-	    MODULE_NOTIFICATIONS.notify({
-	        pageWorker : this.worker,
-	        state : 'filled',
-	        site : this.url
-	    });
-	    // TODO check mode
-	    this.validate();
-    }
-
-    this.onValidated = function () {
-    	Logger.debug('alex.class.js', 'onValidated');
-	    this.validated = true;
-	    MODULE_NOTIFICATIONS.notify({
-	        pageWorker : this.worker,
-	        state : 'validated',
-	        site : this.url
-	    });
-    }
-
-    this.onProblem = function (message) {
-    	console.trace();
-    	Logger.debug('alex.class.js', 'onProblem');
-	    MODULE_NOTIFICATIONS.notify({
-	        pageWorker : this.worker,
-	        state : 'error',
-	        message : message,
-	        site : this.url
-	    });
-    }
-    
-
-    this.worker.port.on('problem', this.onProblem);
-    this.worker.port.on('filled', this.onFilled);
-    this.worker.port.on('validated', this.onValidated);
+    var instance = this;
+	this.worker.port.on('problem', function (data) {
+		instance.onProblem(data);
+	});
+	this.worker.port.on('filled', function (data) {
+		instance.onFilled(data);
+	});
+	this.worker.port.on('validated', function (data) {
+		instance.onValidated(data);
+	});
 }
 
-// Widget constructor
-exports.Alex = function (script, worker) {
-	Logger.debug('alex.class.js', 'exports.Alex');
-	return new AlexClass(script, worker);
+Alex.prototype.fillForm = function() {
+	Logger.debug('alex.class.js', 'fillForm');
+	var theWorker = this.worker;
+    PASSWORD_MANAGER.getFirst({
+	    url : this.script.site
+    }, function(credential) {
+    	theWorker.port.emit('fillForm', credential);
+    });
+}
+
+Alex.prototype.validate = function() {
+	Logger.debug('alex.class.js', 'validate');
+	this.worker.port.emit('validate');
+}
+
+Alex.prototype.onFilled = function() {
+	Logger.debug('alex.class.js', 'onFilled');
+    this.filled = true;
+    MODULE_NOTIFICATIONS.notify({
+        pageWorker : this.worker,
+        state : 'filled',
+        site : this.site
+    });
+
+    var modeAuto = true;
+    if(!!STORAGE[this.script.id]) {
+    	modeAuto = (STORAGE[this.script.id].mode == 'auto');
+    }
+    
+    if(modeAuto) {
+        this.validate();
+    }
+}
+
+Alex.prototype.onValidated = function() {
+	Logger.debug('alex.class.js', 'onValidated');
+    this.validated = true;
+    MODULE_NOTIFICATIONS.notify({
+        pageWorker : this.worker,
+        state : 'validated',
+        site : this.site
+    });
+}
+
+Alex.prototype.onProblem = function(message) {
+	Logger.debug('alex.class.js', 'onProblem');
+    MODULE_NOTIFICATIONS.notify({
+        pageWorker : this.worker,
+        state : 'error',
+        message : message,
+        site : this.site
+    });
 }
