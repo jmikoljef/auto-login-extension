3776315666d0bbe1b8daf8b950397581ae1c3cc4
pierremarie.dhaussy <pierremarie.dhaussy@b6b17452-1c8b-ef94-15eb-1b5c84adfae8>
2012-04-30 19:41:45 +0000

    File convention, take in charge

--- trunk/firefox/src/lib/module-localization.js
+++ trunk/firefox/src/lib/module-localization.js
@@ -1,6 +1,6 @@
 Locale = require("api-utils/l10n/locale");
 Data = require("self").data;
-I18n = require('third-libs-loader').load('i18n', [{code:'var XMLHttpRequest = require(\'xhr\').XMLHttpRequest;'}, {path:'file/file.js'}, {path:'i18n/i18n.js'}]);
+I18n = require('third-libs-loader').load('I18n', [{code:'var XMLHttpRequest = require(\'xhr\').XMLHttpRequest;'}, {path:'file/file.class.js'}, {path:'i18n/i18n.class.js'}]);
 var i18n = new I18n(Locale.getPreferedLocales(), Data.url);
 
 exports.get = function(message) {
