5420c409a4dbcde864e3bd2957b7b50b8271f2ea
Bruno Macherel <Bruno Macherel@b6b17452-1c8b-ef94-15eb-1b5c84adfae8>
2012-05-02 22:35:37 +0000

    Ajout de la licence dans les entêtes

--- common/scripts/caisseepargne/script.js
+++ common/scripts/caisseepargne/script.js
@@ -1,3 +1,23 @@
+/*
+ * Auto Login Extension or ALEx, allows you to easily log in on websites
+ * with tiresome security systems, such as banks.  
+ * Copyright (C) 2012 Bruno Macherel, Pierre-Marie Dhaussy, Aurélie Gandour
+ * <auto-login-extension@googlegroups.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  See LICENSE.txt or <http://www.gnu.org/licenses/  >.
+ */
+
 USERNAME_INPUT_ID = 'ctl01_CC_ind_pauthpopup_ctl01_CC_ind_ident_ctl01_CC_ind_inputuserid_sup_txnuabbd';
 USERNAME_LINK_ID = 'ctl01_CC_ind_pauthpopup_ctl01_CC_ind_ident_ctl01_CC_ind_inputuserid_sup_btnValider';
 PASSWORD_INPUT_ID = 'codconf';
